658a54545ede4598fad9f6c6ae4a07ec0d9ac4ac
Peter <peteraburland@gmail.com>
2016-10-09 21:00:50 +0100

    Create Question1.c

---
+++ src/Chapter-2-C99/Exercises/Question1.c
@@ -0,0 +1,11 @@
+//Create and run Kernighan and Ritchie's famous "Hello, World!" program
+//Do you get a warning message from the compiler? If so, whats needed to make it
+//go away
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world\n");
+}
+
+//Answer: It compiled and I didnt get any warnings. Using GCC from codeblocks  
