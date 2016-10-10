09cde271a4972216485730b549ca08a66259dbdf
Peter <peteraburland@gmail.com>
2016-10-10 20:47:20 +0100

    Create Question4.c

---
+++ src/Chapter-2-C99/Exercises/Question4.c
@@ -0,0 +1,13 @@
+//Write a program that asks the user to enter a dollars-and-cents
+//amount, then displays the amount with 5% tax added:
+//Enter an amount: 100.00
+//With tax added: $105.00
+#include <stdio.h>
+int main(void)
+{
+    float amount;
+    printf("Enter an amount: ");
+    scanf("%f", &amount);
+    printf("With tax added: %.2f", amount*1.05);
+    return 0;
+}
