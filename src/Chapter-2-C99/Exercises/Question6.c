fe597fe0941fb978b42b9c8547f1a6ecaaafbac7
Peter <peteraburland@gmail.com>
2016-10-10 21:09:44 +0100

    Create Question6.c

---
+++ src/Chapter-2-C99/Exercises/Question6.c
@@ -0,0 +1,15 @@
+//Modify the program of Programming Project 5 so that the polynomial
+//is evaluated using the following formula:
+//((((3x+2)x-5)x-1)x+7)x-6
+//Note that the modified program performs fewer multiplications. This
+//technique for evaluating polynomials is known as Horner's Rule.
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("Please enter a value of x: ");
+    scanf("%d", &x);
+    printf("Answer is: %d", ((((3*x+2)*x-5)*x-1)*x+7)*x-6);
+    return 0;
+}
