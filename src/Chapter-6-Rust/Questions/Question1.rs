6bc7fc8b7e196f1e3f6437f2b8f2553b50370c81
Peter <peteraburland@gmail.com>
2016-11-05 22:53:21 +0000

    Create Question1.rs

---
+++ src/Chapter-6-Rust/Questions/Question1.rs
@@ -0,0 +1,50 @@
+//Write a program that finds the largest in a series of numbers entered by the
+//user. The program must prompt the user to enter numbers one by one. When the user
+//enters 0 or a negative number, the program must display the largest nonnegative
+//number entered:
+//Enter a number: 60
+//Enter a number: 38.3 etc etc
+#[macro_use] extern crate text_io;
+//process library to pause console
+use std::process::Command;
+//Need this to use the print!
+use std::io::{self, Write};
+
+fn main()
+{
+	let mut largest : f32 = 0.0;
+
+	loop 
+	{
+		print!("Enter a number (0 will exit): ");
+		io::stdout().flush().unwrap();
+		//read in num as text first
+		let mut num = String::new();
+		io::stdin().read_line(&mut num)
+			.ok()
+			.expect("Something went wrong");
+		let num:f32 =  num.trim()
+			.parse()
+			.ok()
+			.expect("Failed to parse number");
+		
+
+		if num == 0.0
+		{
+			io::stdout().flush().unwrap();
+			break;
+		}
+		//else check if new number is larger than current largest
+		else
+		{
+			if num > largest
+			{
+				largest = num;
+			}
+		}
+	}
+	print!("The largest number entered was {}\n", largest);
+	
+	io::stdout().flush().unwrap();
+	let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
+}
