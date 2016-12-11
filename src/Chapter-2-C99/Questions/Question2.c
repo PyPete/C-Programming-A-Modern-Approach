06d5f604b9328c7892932e5efd4af98decc99388
Peter <peteraburland@gmail.com>
2016-12-11 15:29:54 +0000

    Create Question2.c

---
+++ src/Chapter-2-C99/Questions/Question2.c
@@ -0,0 +1,23 @@
+//Consider the following program
+//#include <stdio.h>
+//int main(void)
+//{
+//  printf("Parkinson's Law:\bWork expands so as to ");
+//  printf("fill the time\n");
+//  printf("available for its completion.\n");
+//  return 0;
+//}
+//a)Identify the directives and statements in this program.
+//b)What output does the program produce? Will print text over three lines
+
+
+#include <stdio.h> //directive
+
+int main(void)
+{
+    //Statements
+    printf("Parkinson's Law:\bWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
