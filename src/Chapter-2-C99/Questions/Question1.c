ad2c8301f3f945701951b0b16cf4b9da41935464
Peter <peteraburland@gmail.com>
2016-12-11 15:22:59 +0000

    Create Question1.c

---
+++ src/Chapter-2-C99/Questions/Question1.c
@@ -0,0 +1,10 @@
+//Create and run Kernighan and Ritchie's famous "Hello, world" program:
+
+//Do you get a warning message from the compiler? No - but must be something to do
+//we no return value
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
