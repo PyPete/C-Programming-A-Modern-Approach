29a76103b78fe77320015f71b524abe4ac18801a
Peter <peteraburland@gmail.com>
2016-12-23 23:03:17 +0000

    Create Question3.c

---
+++ src/Chapter 13 C99/Questions/Question3.c
@@ -0,0 +1,39 @@
+//Deals a random hand of cards
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_SUITS 4
+#define NUM_RANKS 13
+
+int main(void)
+{
+    //initialise variables
+    bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+    int num_cards, rank, suit;
+    const char *rank_code[] = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+                              "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+    const char *suit_code[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    //create random
+    srand((unsigned)time(NULL));
+    //ask users to enter number of hands
+    printf("Enter number of cards in hand: ");
+    scanf("%d", &num_cards);
+    //your hand
+    printf("Your hand: \n");
+    while (num_cards > 0)
+    {
+        suit = rand() % NUM_SUITS; //picks a random suit
+        rank = rand() % NUM_RANKS; //picks a random rank
+        if (!in_hand[suit][rank])
+        {
+            in_hand[suit][rank] = true;
+            num_cards--;
+            printf("%s of %s\n", rank_code[rank], suit_code[suit]);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
