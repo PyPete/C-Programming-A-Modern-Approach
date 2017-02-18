838b96b8401e4fefc10bb14f96bc34641fa49e2d
PyPete <peteraburland@gmail.com>
2017-02-18 22:26:00 +0000

    Changed

--- src/Chapter-2-C99/Questions/Question3.c
+++ src/Chapter-2-C99/Questions/Question3.c
@@ -1,12 +1,14 @@
 //Condense the dweight.c program by:
-//1) replacing the assignments to heigh, length, and width with initialisers
-//2) removing the weight variable, instead calculating (volumen+165)/166 with the
+//1) replacing the assignments to height, length, and width with initialisers
+//2) removing the weight variable, instead calculating (volume+165)/166 with the
 //last printf
 #include <stdio.h>
 int main(void)
 {
-    int height=8, length=12, width=10, volume, weight;
+    //Variables initialisers
+    int height=8, length=12, width=10, volume;
     volume = height * length * width;
+    //print statements
     printf("Dimensions: %dx%dx%d\n", length, width, height);
     printf("Volume (cubic inches): %d\n", volume);
     printf("Dimensional weight (pounds): %d\n", (volume + 165) / 166);
